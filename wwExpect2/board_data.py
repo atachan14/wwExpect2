13b2d2414849e831dc46d58a50f6d8dcf983db2c
WIN-1Q8DR817JKE\user <aklius14pg@gmail.com>
2024-12-21 20:38:47 +0900

    やっぱりギブアップっぽい。

--- wwExpect2/board_data.py
+++ wwExpect2/board_data.py
@@ -2,20 +2,46 @@ import session_data
 from collections import defaultdict
 from roles import *
 
+
 class Board:
-    def __init__(self, fase, cs, player_list):
+    def __init__(self, fase, cs, os, player_list):
         self.fase = fase
         self.cs = cs
+        self.os = os
         self.player_list = player_list
-        self.co_group_dict = defaultdict(list)
 
-        # player_list をループして分類
+        self.co_group_dict = {}
+        self.latent_group
+
+        # setup_co_group
+        self.co_list_dict = defaultdict(list)
         for player in player_list:
-            if isinstance(player.co, Latent):  # Latentの場合
-                self.latent_group_
-                self.co_group_dict[player.co].append(player)
+            self.co_list_dict[player.co].append(player)
 
-        
+        for co in self.co_list_dict:
+            if not isinstance(co, Latent):
+                self.co_group_dict[co] = CoGroup(
+                    self, co, self.co_list_dict[co])
+
+        for co in self.co_list_dict:
+            if isinstance(co, Latent):
+                self.latent_group = LatentGroup(
+                    self, co, self.co_list_dict[co])
+
+
+class CoGroup:
+    def __init__(self, b, co, player_list):
+        self.co = co
+        self.player_list = player_list
+
+        self.size = b.cs.role_regulation[co]
+        self.vacate = self.size - len(player_list)
+
+
+class LatentGroup(CoGroup):
+    def __init__(self, b, co, player_list):
+        super().__init__(co, player_list)
+
 
 class Player:
     def __init__(self, index, name=None, alive=True, co=Latent()):
@@ -24,16 +50,16 @@ class Player:
         self.alive = alive
         self.co = co
         self.ability_result = {}  # role:result あとで実装
-        
-        #ここまでがユーザーの入力や前回のボードから引き継ぐ情報
-        #以降のパーセンテージは別の関数で後から追加する
+
+        # ここまでがユーザーの入力や前回のボードから引き継ぐ情報
+        # 以降のパーセンテージは別の関数で後から追加する
 
     def __str__(self):
         return self.name
 
     def __repr__(self):
         return self.name
-    
+
     #     self.setup_ability_result()
 
     # def setup_ability_result():
--- wwExpect2/main.py
+++ wwExpect2/main.py
@@ -4,21 +4,19 @@ import input_data
 import session_data
 
 
+#初回のみの処理____
 ri = input_data.RegulationInput
 cs = session_data.CastSetting(ri)
+os = session_data.OptionSetting(ri)
 
-first_player_list=[]
+first_player_list = []
 for i in range(cs.player_size):
-    first_player_list.append(Player(i+1,Latent))
+    first_player_list.append(Player(i + 1, Latent))
 
-#debug
-# for player in first_player_list:
-#     print(player.index,player.co.name)
-
-first_board=Board(1.0,cs,first_player_list)
-#debug
+first_board = Board(1.0, cs, os,first_player_list)
+# debug
 # print(first_board.player_list)
 
+#____ブラウザに送信して終わり
 
-
-
+#2回目以降の処理
